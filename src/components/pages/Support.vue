2e3a86f764125c7a734ad30e9358fea2416f2db7
sebga <seb.gauthier@gmail.com>
2022-11-10 18:34:56 -0500

    Support us page

---
+++ src/components/pages/Support.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="card">
+    <h2>{{L('SupportUs')}}</h2>
+    <p>You can support us and this project by delegating to the <a :href="canadaStakesUrl" target="_blank">CanadaStakes CAN1 pool</a></p>
+
+    
+  </div>
+  <div>
+    
+  </div>
+</template>
+
+<script setup>
+const canadaStakesUrl = import.meta.env.VITE_CANADA_STAKES_URL
+</script>
