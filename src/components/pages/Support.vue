a66f64aa7ea14d9127441045d5a65454a40dcc63
sebga <seb.gauthier@gmail.com>
2022-11-11 18:02:26 -0500

    translations

--- src/components/pages/Support.vue
+++ src/components/pages/Support.vue
@@ -1,13 +1,9 @@
 <template>
   <div class="card">
-    <h2>{{L('SupportUs')}}</h2>
-    <p>You can support us and this project by delegating to the <a :href="canadaStakesUrl" target="_blank">CanadaStakes CAN1 pool</a></p>
-
-    
+    <h2>{{ L('SupportUs') }}</h2>
+    <p>{{ L('You can support us text') }} <a :href="canadaStakesUrl" target="_blank">CanadaStakes CAN1 pool</a></p>
   </div>
-  <div>
-    
-  </div>
+  <div></div>
 </template>
 
 <script setup>
