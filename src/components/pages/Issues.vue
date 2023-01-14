ed785d9d31de7e8788db554d4b512687c92c94b5
Jonny <jonny.trafford@gmail.com>
2023-01-14 14:30:51 -0500

    added disclaimer text

--- src/components/pages/Issues.vue
+++ src/components/pages/Issues.vue
@@ -1,8 +1,9 @@
 <template>  
     <div class="card">
       <h2>{{ L('Issues') }}</h2>
+      <p>{{ L('Disclaimer Text') }}</p>      
       <p>{{ L('Bug Report Text') }} <a :href="repositoryUrl" target="_blank">Github</a></p>
-    </div>
+    </div>    
     <div></div>
   </template>
   
