958a481751610475f569c6c5707e96e3736651ee
Sebastien Gauthier <44874281+sebgauthier@users.noreply.github.com>
2023-05-19 08:06:21 -0400

    58 update adaview search to use new post method (#59)
    * using async/await... and posting to get URL save wallet address first.
    
    Not complete, but committing this so Jonny can test CORS issues.
    
    * updated useFetchWallet to POST to proper URL
    
    * 1. Prior to fetching a wallet, POST to get the proper wallet identifier first.
    2. Use the wallet.__identifier__ property wherever possible.
    3. Fixed an issue where transactions / wallets would sometimes be fetched twice.
    
    ---------
    
    Co-authored-by: Jonny <jonny.trafford@gmail.com>

--- src/composables/useFetchWallet.js
+++ src/composables/useFetchWallet.js
@@ -10,7 +10,7 @@ export const useFetchWallet = () => {
   let abortController = new AbortController()
   const { network } = useSettings()
 
-  const fetchWallet = (address) => {
+  const fetchWallet = async (address) => {
     // send an abort signal if there's already a request happening
     if (loading.value) {
       abortController.abort()
@@ -22,24 +22,24 @@ export const useFetchWallet = () => {
     error.value = null
     wallet.value = null
 
-    axios
-      .get(`${network.value.url}/wallets/${address}`, { signal: abortController.signal })
-      .then((response) => {
-        wallet.value = response.data.data
-        loading.value = false
-      })
-      .catch((err) => {
-        if (axios.isCancel(err)) {
-          // Request cancelled
-          error.value = null
-          return
-        } else if (err.response.data) {
-          error.value = { message: err.response.data.message + ' (' + err.response.data.network + ')' }
-        } else {
-          error.value = { message: err.message }
-        }
-        loading.value = false
-      })
+    try {
+      const urlSafeResponse = await axios.post(`${network.value.url}/wallets`, { address })
+      const walletIdentifier = urlSafeResponse.data.walletIdentifier
+      const response = await axios.get(`${network.value.url}/wallets/${walletIdentifier}`, { signal: abortController.signal })
+      wallet.value = response.data.data
+      loading.value = false
+    } catch (err) {
+      if (axios.isCancel(err)) {
+        // Request cancelled
+        error.value = null
+        return
+      } else if (err.response?.data) {
+        error.value = { message: err.response.data.message + ' (' + err.response.data.network + ')' }
+      } else {
+        error.value = { message: err.message }
+      }
+      loading.value = false
+    }
   }
 
   return {
